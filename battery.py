736f302c0a7e2e8985d9494ff02c4fc9fe565cb4
GSG Robots <gsgrobots@gmail.com>
2023-02-23 15:13:10 +0100

    Now stops automatically there is no unnecessary while-True anymore

--- battery.py
+++ battery.py
@@ -3,7 +3,8 @@
 import hub # type: ignore # pylint: disable=import-error
 import spike
 
-while True:
-    CAPACITY = hub.battery.capacity_left()
-    MSG = str(CAPACITY)+"%"
-    spike.PrimeHub().light_matrix.write(MSG)
+CAPACITY = hub.battery.capacity_left()
+MSG = str(CAPACITY)+"%"
+spike.PrimeHub().light_matrix.write(MSG)
+
+raise SystemExit
