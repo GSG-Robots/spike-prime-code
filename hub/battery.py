f2dadf54464e7109cdecc471139ff7b91885ee7c
GSG Robots <124367764+GSG-Robots@users.noreply.github.com>
2023-12-14 15:50:39 +0100

    Added representation to avoid pylint errors

---
+++ hub/battery.py
@@ -0,0 +1,16 @@
+"""
+Values and functions regarding hub battery
+"""
+
+
+def capacity_left():
+    """
+    Get left capacity. [Defaults to 80 when run on PC.]
+    """
+    return 80
+
+def charger_detect():
+    """
+    Get if charging. [Defaults to True when run on PC.]
+    """
+    return True
