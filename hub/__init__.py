f0a7d43a7c2ca7e8ea4d92976434e6a86452d28a
GSG Robots <124367764+GSG-Robots@users.noreply.github.com>
2023-12-14 17:14:26 +0100

    Updated mappings

---
+++ hub/__init__.py
@@ -0,0 +1,12 @@
+"""
+LEGO provided functions and values.
+"""
+
+
+def power_off(fast=True, restart=False) -> None: # pylint: disable=unused-argument
+    """Power off the hub
+
+    Args:
+        fast (bool, optional): Whether to stop fast. Defaults to True.
+        restart (bool, optional): Whether to restart afterwards. Defaults to False.
+    """
