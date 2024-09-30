0cb05a9612d763d18b01669bf8c32da2d292d0af
GSG Robots <124367764+GSG-Robots@users.noreply.github.com>
2024-09-30 15:34:45 +0200

    Nevermind

--- src/runs/run_2.py
+++ src/runs/run_2.py
@@ -1,6 +1,5 @@
 from gsgr.conditions import Sec
 from gsgr.movement import gyro_drive
-from gsgr.configuration import config as cnfg
 
 
 metadata = {"display_as": 2, "color": "green"}
@@ -8,5 +7,4 @@ config = {}
 
 
 def run():
-    print(cnfg.debug_mode)
     gyro_drive(80, 0, Sec(10), accelerate_for=5, decelerate_for=5)
