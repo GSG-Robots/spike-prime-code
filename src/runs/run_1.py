614e9d46e6d0c73f971c1c8c4645dc2c62798c93
GSG-Robots <gsgrobots@gmail.com>
2024-12-23 19:40:31 +0100

    fix import :(

--- src/runs/run_1.py
+++ src/runs/run_1.py
@@ -1,7 +1,7 @@
 import time
 from gsgr.conditions import sec, deg, cm, THEN
 from gsgr.enums import Color, Attachment
-from gsgr.movement import gyro_set_origin, gyro_drive, gyro_turn, run_attachment, hold_attachment, run_attachment_degrees, free_attachment
+from gsgr.movement import gyro_set_origin, gyro_drive, gyro_turn, run_attachment, hold_attachment, free_attachment
 
 display_as = 1
 color = Color.GREEN
