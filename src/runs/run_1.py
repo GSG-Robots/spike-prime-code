e918dd5e2e92a5c46b99fae0a5a4974577d7c78a
GSG-Robots <gsgrobots@gmail.com>
2025-01-26 15:43:46 +0100

    progress: run 1

--- src/runs/run_1.py
+++ src/runs/run_1.py
@@ -18,7 +18,14 @@ def run():
     # Set Gyro Origin
     gyro_set_origin()
 
-    gyro_drive(0, 50, cm(49))
+    gyro_drive(
+        0,
+        50,
+        cm(50),
+        p_correction=1.4,
+        i_correction=0,
+        d_correction=-0.7,
+    )
     run_attachment(Attachment.BACK_LEFT, 100, 2, False, True)
     run_attachment(Attachment.FRONT_RIGHT, -100, 3, False, True)
     run_attachment(Attachment.FRONT_LEFT, 50, 1)
