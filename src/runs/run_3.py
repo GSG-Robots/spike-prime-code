03ba9c0e06fc7d265be33fe917329f0bf63705db
GSG-Robots <gsgrobots@gmail.com>
2025-01-26 19:13:47 +0100

    progress: run 3

--- src/runs/run_3.py
+++ src/runs/run_3.py
@@ -1,5 +1,5 @@
 import time
-from gsgr.conditions import sec, deg, cm, THEN
+from gsgr.conditions import sec, deg, cm, THEN, line
 from gsgr.enums import Color, Attachment
 from gsgr.movement import (
     gyro_set_origin,
@@ -19,37 +19,25 @@ def run():
     # Set Gyro Origin
     gyro_set_origin()
 
-    run_attachment(Attachment.FRONT_RIGHT, -50, 1)
-    free_attachment(Attachment.FRONT_RIGHT)
-
     gyro_drive(0, 40, cm(15))
 
     # time.sleep(0.3)
 
-    gyro_drive(0, 70, cm(80), i_correction=3, accelerate_for=cm(10))
-    
-    gyro_drive(0, 40, cm(10), i_correction=3)
+    gyro_drive(0, 70, cm(80), p_correction=1.7, d_correction=-0.7) # cm(80),
 
-    # gyro_turn(0, 20)
-
-    run_attachment(Attachment.FRONT_LEFT, 15, 2)
-
-    for _ in range(8):
+    for _ in range(10):
         gyro_drive(0, -10, cm(1))
 
         run_attachment(Attachment.FRONT_LEFT, 50, 0.2)
 
     time.sleep(.5)
 
-    run_attachment(Attachment.FRONT_RIGHT, 100, 2.5)
-
-    gyro_turn(-90, 45)
-
-    gyro_drive(-90, -90, cm(10))
-
-    gyro_drive(-90, 50, cm(20))
+    gyro_drive(0, -90, cm(25))
     
-    gyro_turn(0, 45)
-    
-    gyro_drive(0, 50, cm(30))
-    gyro_drive(0, 100, cm(50))
+    # Zurück nach Hause
+    gyro_turn(-50, 70)
+    gyro_drive(-50, 70, cm(30))    
+    gyro_turn(-10, 70)
+    gyro_drive(-10, 70, cm(30))
+    gyro_turn(15, 70)
+    gyro_drive(15, 100, cm(40))
