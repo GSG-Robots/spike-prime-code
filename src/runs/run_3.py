22056765504b74e2a403b42cfd86542a685f0cb9
GSG-Robots <gsgrobots@gmail.com>
2025-01-27 10:30:23 +0100

    progress: run 3

--- src/runs/run_3.py
+++ src/runs/run_3.py
@@ -19,25 +19,24 @@ def run():
     # Set Gyro Origin
     gyro_set_origin()
 
-    gyro_drive(0, 40, cm(15))
+    gyro_drive(0, 40, cm(20))
 
     # time.sleep(0.3)
 
-    gyro_drive(0, 70, cm(80), p_correction=1.7, d_correction=-0.7) # cm(80),
+    gyro_drive(0, 70, cm(75), accelerate_for=cm(10)) # , p_correction=1.7, d_correction=-0.7
 
-    for _ in range(10):
-        gyro_drive(0, -10, cm(1))
-
+    for _ in range(8):
         run_attachment(Attachment.FRONT_LEFT, 50, 0.2)
+        gyro_drive(0, -10, cm(1))
 
     time.sleep(.5)
 
-    gyro_drive(0, -90, cm(25))
+    gyro_drive(0, -90, cm(19))
     
     # Zurück nach Hause
     gyro_turn(-50, 70)
-    gyro_drive(-50, 70, cm(30))    
+    gyro_drive(-50, 70, cm(20))    
     gyro_turn(-10, 70)
-    gyro_drive(-10, 70, cm(30))
+    gyro_drive(-10, 70, cm(37))
     gyro_turn(15, 70)
-    gyro_drive(15, 100, cm(40))
+    gyro_drive(15, 100, cm(60))
