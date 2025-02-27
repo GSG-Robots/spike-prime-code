3dff30d50b28b01880ce4adb8c36a0712d0e168d
GSG-Robots <gsgrobots@gmail.com>
2025-02-27 18:08:44 +0100

    - progress: run 4 - progress: run 5 - adjust min speed for turning

--- src/runs/run_4.py
+++ src/runs/run_4.py
@@ -1,11 +1,11 @@
-from gsgr.conditions import cm
-from gsgr.enums import Attachment, Color
+from gsgr.conditions import cm, sec
+from gsgr.enums import Color
 from gsgr.movement import (
-    free_attachment,
     gyro_drive,
+    gyro_turn,
     gyro_set_origin,
-    run_attachment,
 )
+import time
 
 display_as = 4
 color = Color.RED
@@ -14,21 +14,8 @@ color = Color.RED
 def run():
     # Set Gyro Origin
     gyro_set_origin()
-    gyro_drive(0, -80, cm(5))
 
-    # Plankton yeeten
-    run_attachment(Attachment.BACK_LEFT, -85, 1, stall=True, untension=True)
-
-    # Arm homen
-    run_attachment(Attachment.BACK_LEFT, 50, 1, stall=True, untension=True)
-
-    # Zu Quadropus fahren
-    gyro_drive(1, -70, cm(38), decelerate_for=cm(12), decelerate_from=cm(30))
-
-    # Sachen sammeln
-    run_attachment(Attachment.BACK_LEFT, -30, 1.5)
-
-    # Zurück zu base
-    gyro_drive(0, 50, cm(60), p_correction=0.3)
-
-    free_attachment(Attachment.BACK_LEFT)
+    gyro_drive(0, -60, cm(50), decelerate=10)
+    time.sleep(1)
+    gyro_drive(0, 75, sec(5))
+    gyro_turn(20, 150)
