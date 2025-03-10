614beab09087f8d1738c02fcb5de3bd9cda32aa8
GSG-Robots <gsgrobots@gmail.com>
2025-03-10 11:37:43 +0100

    progress: run 2

--- src/runs/run_2.py
+++ src/runs/run_2.py
@@ -16,54 +16,25 @@ color = Color.WHITE
 
 # FEATURE FLAGS
 # x Sekunden
-FEATURE_KORALLEN = True
+FEATURE_KORALLEN = False
 # #############
 
 
 # Gesamt: x Sekunden
 def run():
-
-    # Anbaute homen
-    # if FEATURE_KORALLEN:
-        # run_attachment(Attachment.FRONT_RIGHT, 100, 1.3, untension=False)
-        # run_attachment(Attachment.FRONT_RIGHT, -100, 1, False, True)
-    run_attachment(
-        Attachment.FRONT_LEFT, 75, 1, stall=True, untension=0
-    )
-    # hold_attachment(Attachment.FRONT_LEFT, False)
-    
     # Set Gyro Origin
-    gyro_drive(0, -50, sec(1))
     gyro_set_origin()
-    # hold_attachment(Attachment.FRONT_LEFT, True)
-
-    # Fahre zu Schiff
-    gyro_drive(0, 65, cm(12))
-    gyro_turn(45, 110, Pivot.RIGHT_WHEEL, min_speed=4)
-    gyro_drive(45, 65, cm(17.5))
-    gyro_turn(0, 90, Pivot.LEFT_WHEEL, min_speed=2)
-    gyro_drive(0, 65, cm(11))
-
-    # Sachen einsammeln
-    run_attachment(Attachment.FRONT_LEFT, -95, 1.5, True, untension=90)
-    hold_attachment(Attachment.BACK_RIGHT, await_completion=False)
-    gyro_drive(0, 20, cm(5.5))
-
-    # Mast stellen
-    run_attachment(
-        Attachment.BACK_RIGHT, 100, 3.7, stall=True
-    )
-
-    if FEATURE_KORALLEN:
-        run_attachment(
-            Attachment.BACK_RIGHT, -75, 1.8
-        )
-        run_attachment(Attachment.FRONT_RIGHT, -100, 2, stall=True)
-
-        # gyro_turn(-10, 120, Pivot.LEFT_WHEEL, min_speed=2)
-        gyro_drive(0, 70, cm(11))
-        time.sleep(0.75)
-        gyro_drive(0, -70, cm(7))
-
-    gyro_drive(45, -100, sec(3))
-    gyro_turn(-45, 150, Pivot.LEFT_WHEEL)
+    gyro_drive(0, 70, cm(30), accelerate=5, decelerate=5)
+    gyro_turn(-35, 90, Pivot.LEFT_WHEEL)
+    gyro_drive(-35, 70, cm(20), accelerate=5, decelerate=5)
+    run_attachment(Attachment.FRONT_LEFT, -100, 1.5, stall=True, untension=180)
+    gyro_turn(-10, 90, Pivot.RIGHT_WHEEL)
+    gyro_turn(-35, 90, Pivot.LEFT_WHEEL)
+    gyro_drive(-35, 40, cm(3), accelerate=5, decelerate=5)
+    run_attachment(Attachment.FRONT_RIGHT, -20, 4.5, stall=True)
+    run_attachment(Attachment.FRONT_RIGHT, 80, 1, stall=True, untension=180)
+    # gyro_turn(-25, 90, Pivot.LEFT_WHEEL)
+    # gyro_turn(-35, 90, Pivot.RIGHT_WHEEL)
+    gyro_drive(-35, -70, cm(22), accelerate=5, decelerate=5)
+    gyro_turn(-125, 90, Pivot.LEFT_WHEEL)
+    gyro_drive(-125, -30, cm(10), accelerate=5, decelerate=25)
