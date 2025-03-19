61641defb561f83c6204d5aab5b7281003ed8f80
GSG-Robots <gsgrobots@gmail.com>
2025-03-19 17:48:26 +0100

    - progress run 1 - progress run 2 - progress run 4 - used run 6 for "LIVE CHALLENGE"

--- src/runs/run_4.py
+++ src/runs/run_4.py
@@ -1,7 +1,7 @@
 import time
 
-from gsgr.conditions import cm, sec
-from gsgr.enums import Attachment, Color
+from gsgr.conditions import OR, cm, sec
+from gsgr.enums import Attachment, Color, Pivot
 from gsgr.movement import gyro_drive, gyro_set_origin, gyro_turn, run_attachment
 
 display_as = 4
@@ -12,8 +12,13 @@ def run():
     # Set Gyro Origin
     gyro_set_origin()
 
-    gyro_drive(0, -60, cm(50), decelerate=10)
-    time.sleep(1)
-    run_attachment(Attachment.BACK_LEFT, -100, 5, stall=False, await_completion=False)
-    gyro_drive(0, 75, sec(5))
-    gyro_turn(20, 150)
+    gyro_drive(0, 60, cm(65), accelerate=5, decelerate=5)
+    run_attachment(Attachment.BACK_RIGHT, -100, 2)
+    run_attachment(Attachment.FRONT_RIGHT, -100, 4.5)
+    gyro_drive(0, 60, cm(5), accelerate=5, decelerate=5)
+    run_attachment(Attachment.FRONT_LEFT, -50, 2.5)
+    gyro_drive(0, -60, cm(40), accelerate=5, decelerate=5)
+    gyro_turn(-45, 100, Pivot.RIGHT_WHEEL)
+    gyro_drive(-45, 40, OR(cm(35), sec(5)), accelerate=5, decelerate=15)
+    gyro_drive(-45, -100, OR(cm(40), sec(10)), accelerate=5)
+    
--- src/runs/run_6.py
+++ src/runs/run_6.py
@@ -1,8 +1,9 @@
 from gsgr.conditions import cm
-from gsgr.enums import Color
+from gsgr.enums import Color, Pivot
 from gsgr.movement import (
     gyro_drive,
     gyro_set_origin,
+    gyro_turn
 )
 
 display_as = "N"
@@ -11,4 +12,11 @@ color = Color.ORANGE
 
 def run():
     gyro_set_origin()
-    gyro_drive(-5, 65, cm(110))
+    # gyro_drive(-5, 65, cm(110))
+    gyro_drive(0, 70, cm(70))
+    gyro_turn(45, 90, Pivot.RIGHT_WHEEL)
+    gyro_drive(45, 70, cm(20))
+    gyro_turn(90, 90, Pivot.RIGHT_WHEEL)
+    gyro_drive(90, 70, cm(50))
+    gyro_turn(90, 90, Pivot.LEFT_WHEEL)
+    
