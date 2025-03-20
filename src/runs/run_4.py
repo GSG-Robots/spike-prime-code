46b2ddbbbbd90732e606d180fc6a7ffd55b7fca7
GSG-Robots <gsgrobots@gmail.com>
2025-03-20 19:17:35 +0100

    - make run 4 like before

--- src/runs/run_4.py
+++ src/runs/run_4.py
@@ -12,13 +12,5 @@ def run():
     # Set Gyro Origin
     gyro_set_origin()
 
-    gyro_drive(0, 60, cm(65), accelerate=5, decelerate=5)
-    run_attachment(Attachment.BACK_RIGHT, -100, 2)
-    run_attachment(Attachment.FRONT_RIGHT, -100, 4.5)
-    gyro_drive(0, 60, cm(5), accelerate=5, decelerate=5)
-    run_attachment(Attachment.FRONT_LEFT, -50, 2.5)
-    gyro_drive(0, -60, cm(40), accelerate=5, decelerate=5)
-    gyro_turn(-45, 100, Pivot.RIGHT_WHEEL)
-    gyro_drive(-45, 40, OR(cm(35), sec(5)), accelerate=5, decelerate=15)
-    gyro_drive(-45, -100, OR(cm(40), sec(10)), accelerate=5)
-    
+    gyro_drive(0, 80, cm(55), accelerate=5, decelerate=5)
+    gyro_drive(0, -100, cm(55), accelerate=5, decelerate=5)
