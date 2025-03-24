452ff190ef22f53ef37c6a3e9c0d55978ff5d8ce
GSG-Robots <gsgrobots@gmail.com>
2025-03-24 11:36:38 +0100

    run 4: Use new impact and pickup

--- src/runs/run_4.py
+++ src/runs/run_4.py
@@ -1,6 +1,6 @@
 import time
 
-from gsgr.conditions import OR, cm, sec
+from gsgr.conditions import OR, cm, impact, pickup, sec, impact
 from gsgr.enums import Attachment, Color, Pivot
 from gsgr.movement import gyro_drive, gyro_set_origin, gyro_turn, run_attachment
 
@@ -12,5 +12,5 @@ def run():
     # Set Gyro Origin
     gyro_set_origin()
 
-    gyro_drive(0, 80, cm(55), accelerate=5, decelerate=5)
-    gyro_drive(0, -100, cm(55), accelerate=5, decelerate=5)
+    gyro_drive(0, 80, impact(cm(55)), accelerate=5, decelerate=5)
+    gyro_drive(0, -100, pickup(impact(cm(70))), accelerate=5, decelerate=5)
