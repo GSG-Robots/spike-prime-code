2ad678d58d1419a85287a52c76f79258145be060
GSG-Robots <gsgrobots@gmail.com>
2025-03-25 16:44:40 +0100

    - progress run 1 - progress run 2

--- src/runs/run_1.py
+++ src/runs/run_1.py
@@ -1,6 +1,6 @@
 import time
 
-from gsgr.conditions import cm, sec
+from gsgr.conditions import cm, impact, pickup, sec
 from gsgr.enums import Color, Pivot
 from gsgr.movement import gyro_drive, gyro_set_origin, gyro_drive, gyro_turn
 
@@ -13,7 +13,8 @@ def run():
     gyro_drive(0, -30, sec(0.5))
     gyro_set_origin()
 
-    gyro_drive(0, 50, cm(56), accelerate=10, decelerate=5)
+    gyro_drive(0, 50, impact(cm(58)), accelerate=10, decelerate=5)
     time.sleep(1)
-    gyro_drive(0, -50, cm(59), accelerate=12)
-    gyro_turn(-45, 150, Pivot.LEFT_WHEEL)
+    gyro_drive(0, -50, cm(25), accelerate=12)
+    gyro_turn(50, 150, Pivot.LEFT_WHEEL)
+    gyro_drive(50, -50, pickup(cm(35)), accelerate=12)
