e7351c9903afcad9da975156eba3084135b154ae
JoJoJux <johannes@jojojux.de>
2025-10-09 00:05:40 +0200

    Adjust Spielzeug boot order

--- spielzeug/boot.py
+++ spielzeug/boot.py
@@ -1,10 +1,7 @@
-# boot.py -- run on boot to configure USB and filesystem
-# Put app code in main.py
-
 import micropython
-
 import hub
 
 micropython.alloc_emergency_exception_buf(128)
+
 hub.config["hub_os_enable"] = False
 hub.config["hub_os_auto_advertise"] = False
--- spielzeug/main.py
+++ spielzeug/main.py
@@ -1,10 +1,10 @@
 import micropython
-import server
 import uasyncio as asyncio
 
 micropython.alloc_emergency_exception_buf(256)
 
 import hub
 
-if not (hub.button.pressed(hub.button.LEFT) and hub.button.pressed(hub.button.RIGHT)):
+if not (hub.button.pressed(hub.button.LEFT) and hub.button.pressed(hub.button.RIGHT) and hub.button.pressed(hub.button.CONNECT)):
+    import server
     asyncio.run(server.main())
