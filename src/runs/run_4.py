20251153ac08fea7ac9e6d7854cdde68231bfd62
GSGRobots <gsgrobots@gmail.com>
2025-12-02 16:58:23 +0100

    Run 4 ist jetzt besser als wie davor

--- src/runs/run_4.py
+++ src/runs/run_4.py
@@ -1,24 +1,21 @@
 import color as col
 
-from ..gsgr.conditions import cm, pickup
+from ..gsgr.conditions import cm, pickup, wheels_blocked, OR, impact
 from ..gsgr.enums import Attachment
 from ..gsgr.movement import free_attachments, gyro_drive, gyro_turn, gyro_wall_align, run_attachment
+from ..gsgr.config import configure
 
 display_as = 4
 color = col.GREEN
+config = configure()
 
 
 def run():
     # Set Gyro Origin
     gyro_wall_align()
-    gyro_drive(0, 500, cm(52.5), accelerate=10, decelerate=30)
-    gyro_turn(5)
-    gyro_drive(5, 500, cm(19), accelerate=10, decelerate=30)
-    run_attachment(Attachment.FRONT_LEFT, -1000, 1.3, stall=True)
-    gyro_drive(0, -400, cm(4), accelerate=10, decelerate=0)
-    run_attachment(Attachment.FRONT_RIGHT, -500, 0.9, stall=True)
-    gyro_drive(0, -400, cm(1), accelerate=10, decelerate=0)
-    free_attachments()
-    run_attachment(Attachment.BACK_LEFT, -700, 1.75, stall=True, untension=5)
-    gyro_drive(5, -900, cm(30), accelerate=10, decelerate=0)
-    gyro_drive(0, -900, pickup(cm(70)), accelerate=10, decelerate=0)
+    gyro_drive(0, 700, OR(cm(70), wheels_blocked()), accelerate=10, decelerate=30)
+    run_attachment(Attachment.FRONT_RIGHT, -400, 2, untension=5)
+    run_attachment(Attachment.FRONT_LEFT, 700, 0.75, untension=5)
+    gyro_drive(0, -700, cm(7), accelerate=10, decelerate=30)
+    run_attachment(Attachment.BACK_LEFT, -700, 1.75, untension=5)
+    gyro_drive(0, -1000, OR(cm(63), pickup(impact(wheels_blocked()))), accelerate=10, decelerate=30)
