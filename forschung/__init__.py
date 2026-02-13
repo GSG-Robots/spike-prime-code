4e05b9b933f017122926169c853a4d93e2e6a418
JoJoJux <johannes@jojojux.de>
2026-02-13 09:29:53 +0100

    Now using port F for drill

--- forschung/__init__.py
+++ forschung/__init__.py
@@ -7,16 +7,16 @@ import motor
 async def loop():
     try:
         running = True
-        motor.run(hub.port.A, 1000)
+        motor.run(hub.port.F, 1000)
         while True:
             if hub.button.pressed(hub.button.POWER):
                 running = not running
                 if running:
-                    motor.run(hub.port.A, 1000)
+                    motor.run(hub.port.F, 1000)
                 else:
-                    motor.stop(hub.port.A, stop=motor.COAST)
+                    motor.stop(hub.port.F, stop=motor.COAST)
                 while hub.button.pressed(hub.button.POWER):
                     await asyncio.sleep_ms(75)
             await asyncio.sleep_ms(75)
     except asyncio.CancelledError:
-        motor.stop(hub.port.A, stop=motor.COAST)
+        motor.stop(hub.port.F, stop=motor.COAST)
